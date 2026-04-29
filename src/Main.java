8ecd2a8f8edc3c008406b94a93cec344576de052
Fk99-a11y <fk080641@gmail.com>
2026-04-30 00:48:21 +0300

    Implement main method to add expenses

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FinanceController controller = new FinanceController();
+
+        controller.addExpense("Food", 50);
+        controller.addExpense("Transport", 20);
+    }
+}
