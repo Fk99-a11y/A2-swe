b4c581e1879f8a5b24f37fe369547ff7155dd6bf
Fk99-a11y <fk080641@gmail.com>
2026-04-30 00:42:09 +0300

    Create Expense class with properties and constructors

---
+++ src/models/Expense.java
@@ -0,0 +1,15 @@
+public class Expense {
+    private int id;
+    private String title;
+    private double amount;
+
+    public Expense() {}
+
+    public Expense(int id, String title, double amount) {
+        this.id = id;
+        this.title = title;
+        this.amount = amount;
+    }
+
+
+}
