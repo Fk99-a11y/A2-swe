b56082187ec39028888254b4f48d24db57ed3cef
Fk99-a11y <fk080641@gmail.com>
2026-05-04 23:38:59 +0300

    Update Expense.java

--- src/models/Expense.java
+++ src/models/Expense.java
@@ -1,15 +1,24 @@
-public class Expense {
-    private int id;
-    private String title;
-    private double amount;
+package models;
+import java.util.Date;
+/**
+ * Represents money spent by the user on specific categories.
+ * Inherits from the Transaction class.
+ */
 
-    public Expense() {}
+public class Expense extends Transaction {
+    private int categoryId;
 
-    public Expense(int id, String title, double amount) {
-        this.id = id;
-        this.title = title;
-        this.amount = amount;
+    public Expense(int transactionId, double amount, Date date, String notes, int categoryId) {
+        super(transactionId, amount, date, notes);
+        this.categoryId = categoryId;
     }
 
+    public int getCategoryId() {
+        return categoryId;
+    }
 
+    @Override
+    public String getDetails() {
+        return "Expense [ID: " + transactionId + "] | Category: " + categoryId + " | Amount: " + amount + " | Date: " + date;
+    }
 }
