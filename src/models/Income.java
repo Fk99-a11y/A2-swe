e54cec869f7a073c40c615e0bbe63c114409b67f
Fk99-a11y <fk080641@gmail.com>
2026-05-04 23:39:46 +0300

    Create Income class to represent user earnings
    This class represents income transactions and includes methods to retrieve the source and formatted details.

---
+++ src/models/Income.java
@@ -0,0 +1,27 @@
+package models;
+import java.util.Date;
+/**
+ * Represents money earned by the user from various sources.
+ * Inherits from the Transaction class.
+ */
+
+public class Income extends Transaction {
+    private String source;
+
+    public Income(int transactionId, double amount, Date date, String notes, String source) {
+        super(transactionId, amount, date, notes); // بيبعت البيانات للأب
+        this.source = source;
+    }
+    public String getSource() {
+        return source;
+    }
+    /**
+     * Overrides the getDetails method to include the income source.
+     * @return Formatted details of the income.
+     */
+
+    @Override
+    public String getDetails() {
+        return "Income Source: " + source + " | Amount: " + amount + " | Date: " + date;
+    }
+}
